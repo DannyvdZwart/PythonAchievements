57ecb4f30eb7b2d24c666924652a0e5096a241a8
DannyvdZwart <32216@ma-web.nl>
2020-10-07 14:43:21 +0200

    PoemPrinting

---
+++ PoemPrinting.py
@@ -0,0 +1,9 @@
+vers1 = "Mijn naam is Danny Ramon van der Zwart. Ik ben heel apart. Vanaf het begin gestart. En ben nu pas op een kwart."
+
+vers2 = "Sport das waar mijn leven voor staat. Dat je er altijd 200% voor gaat. Altijd on my own. But never being alone."
+
+vers3 = "Bad times come and go. Never wanted it to blow. Always trying to stay true. But everyone f*cks up, they do."
+
+vers4 = "No who was always there for me. But they always try to be. Without lying to you. They never do."
+
+print(vers1, vers2, vers3, vers4)
